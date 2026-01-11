56931af89c0ae87d335ae5e97063313b03d643a7
meepoorkeepo <djfaycel06@gmail.com>
2026-01-11 09:20:40 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log('hello devops');
+console.log('learning CI/CD using github actions');
+
+
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log('starting a test ...');
+setTimeout(()=> console.log('waiting 3 secs ...'),3000)
+console.log('tests complete');
+
+
